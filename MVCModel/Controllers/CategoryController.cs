2d50dcda2ecf272b6659cfec26ff35e6b51421ba
ananya teklu <ananya.meseret@gmail.com>
2021-04-16 15:24:07 -0500

    Add Simple MVC

---
+++ MVCModel/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCModel.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly Data.ApplicationDbContext _db;
+
+        public CategoryController(Data.ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Models.Category> objList = _db.Category;
+            return View(objList);
+        }
+    }
+}
---
+++ MVCModel/Data/Migrations/20210416182732_addCategoryToDatabases.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCModel.Data.Migrations
+{
+    public partial class addCategoryToDatabases : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
